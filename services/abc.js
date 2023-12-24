9f98d4bdb441288b1f06c8a25a6604b359981474
neha0070 <jainnehal286@gmail.com>
2023-12-24 15:46:38 +0530

    deleting unnecssary files

--- services/abc.js
+++
@@ -1,7 +0,0 @@
-const f = () => {
-    require('dotenv').config();
-    console.log('Environment Variables:', process.env);
-    console.log('Credential Key:', process.env.credentialkey);
-    console.log('Port:', process.env.PORT);
-};
-module.exports = f;
